d0c6ef924a89a222bc80783f821bba7b0878fd18
Kulyk Dmytro <lnkvisitor.ts@gmail.com>
2017-01-04 13:35:01 +0200

    Update modules package.

--- config/backend.php
+++ config/backend.php
@@ -20,18 +20,6 @@ return [
             }
         );
     },
-    'routes'    => function (Router $router) {
-        $router->get('', ['uses' => 'TemplateController@index', 'as' => 'index']);
-        $router->post('', ['uses' => 'TemplateController@grid', 'as' => 'grid']);
-
-        $router->get('create', ['uses' => 'TemplateController@create', 'as' => 'create']);
-        $router->post('create', ['uses' => 'TemplateController@store', 'as' => 'store']);
-
-        $router->get('edit/{id}', ['uses' => 'TemplateController@edit', 'as' => 'edit']);
-        $router->post('edit/{id}', ['uses' => 'TemplateController@update', 'as' => 'update']);
-
-        $router->post('delete/{id}', ['uses' => 'TemplateController@destroy', 'as' => 'delete']);
-    },
     'requirejs' => [
         'packages' => [
             'rabbitcms/templates' => [
