ef2e2453f7e8720cb0d40665a121bec62360c9e4
Kulyk Dmytro <lnkvisitor.ts@gmail.com>
2017-02-01 21:20:59 +0200

    update modules.

--- src/Providers/TemplatesModuleProvider.php
+++ src/Providers/TemplatesModuleProvider.php
@@ -1,17 +1,22 @@
 <?php
-
+declare(strict_types=1);
 namespace RabbitCMS\Templates\Providers;
 
 use RabbitCMS\Modules\ModuleProvider;
 use RabbitCMS\Templates\Templates;
 
+/**
+ * Class TemplatesModuleProvider
+ *
+ * @package RabbitCMS\Templates\Providers
+ */
 class TemplatesModuleProvider extends ModuleProvider
 {
     public function register()
     {
         parent::register();
 
-        $this->app->singleton([Templates::class => 'templates'], function () {
+        $this->app->singleton(Templates::class, function () {
             return new Templates(
                 $this->app->make('mailer')
             );
