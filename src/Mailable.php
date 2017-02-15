a78536efd2896afc99b35ddd5603c81dca44a9bc
Kulyk Dmytro <lnkvisitor.ts@gmail.com>
2017-02-15 22:22:43 +0200

    Update send mail.

--- src/Mailable.php
+++ src/Mailable.php
@@ -3,7 +3,9 @@ declare(strict_types = 1);
 namespace RabbitCMS\Templates;
 
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Mail\Mailer as MailerContract;
 use Illuminate\Mail\Mailable as IlluminateMailable;
+use Illuminate\Mail\Message;
 use Illuminate\Queue\SerializesModels;
 
 /**
@@ -20,6 +22,30 @@ abstract class Mailable extends IlluminateMailable
     }
 
     /**
+     * @param MailerContract $mailer
+     */
+    public function send(MailerContract $mailer)
+    {
+        Container::getInstance()->call([$this, 'build']);
+
+        $mailer->raw('', function (Message $message) {
+            $this->buildFrom($message)
+                ->buildRecipients($message)
+                ->buildSubject($message)
+                ->buildAttachments($message)
+                ->runCallbacks($message);
+
+            $view = $this->buildView();
+
+            $message->getSwiftMessage()
+                ->setBody($view['html'], 'text/html');
+
+            $message->getSwiftMessage()
+                ->addPart($view['text'], 'text/plain');
+        });
+    }
+
+    /**
      * @return array
      */
     protected function buildView()
@@ -33,7 +59,7 @@ abstract class Mailable extends IlluminateMailable
 
         return [
             'html'  => $engine->render("html:$template", $data),
-            'plain' => $engine->render("plain:$template", $data),
+            'text' => $engine->render("plain:$template", $data),
         ];
     }
 
